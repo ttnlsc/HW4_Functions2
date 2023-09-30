c49f6857f6a14a583c2eb381b5758da02156d866
Artem Vasilev <artem_vasilev_01@list.ru>
2023-09-30 21:10:50 +0300

    Add function count_aa

--- HW4_Voskoboinikov/ultimate_protein_tools.py
+++ HW4_Voskoboinikov/ultimate_protein_tools.py
@@ -34,3 +34,28 @@ def length_of_protein(seq: str) -> int:
     """
 
     return len(seq)
+
+
+def count_aa(seq: str, *, aminoacids: str = None) -> dict:
+    """
+    Counts the number of given or all amino acids in a protein sequence.
+
+    Arguments:
+    - seq (str): sequence to count amino acids
+    - aminoacids (str): which amino acids to count in sequence
+
+    Return:
+    - dict: a dictionary with amino acids and its count
+    """
+
+    aa_dict_count = {}
+    if (aminoacids is None) or (aminoacids == ''):
+        '''
+        I added an additional condition for user-friendly experience.
+        E.g., we can want to find specific aminoacid, look on result and then look on all aminoacids.
+        Without this condition we have to delete keyword argument, but with it we can only make it empty.
+        '''
+        aminoacids = ''.join(set(seq))
+    for aa in aminoacids:
+        aa_dict_count[aa] = seq.count(aa)
+    return aa_dict_count
