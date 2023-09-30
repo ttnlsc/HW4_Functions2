c7b2e9ab83e55945a41923d2f9106fbe8e40049e
Artem Vasilev <artem_vasilev_01@list.ru>
2023-09-30 21:11:18 +0300

    Add function get_fracture_of_aa

--- HW4_Voskoboinikov/ultimate_protein_tools.py
+++ HW4_Voskoboinikov/ultimate_protein_tools.py
@@ -59,3 +59,30 @@ def count_aa(seq: str, *, aminoacids: str = None) -> dict:
     for aa in aminoacids:
         aa_dict_count[aa] = seq.count(aa)
     return aa_dict_count
+
+
+def get_fracture_of_aa(seq: str, *, show_as_percentage: bool = False, aminoacids: str = None) -> dict:
+    """
+    Returns the fracture or percentage of amino acids in a protein sequence.
+
+    Arguments:
+    - seq (str): sequence in which you need to calculate the fracture of amino acids
+    - show_as_percentage (bool): change it to True, if you want to get results with percentages
+    - aminoacids (str): the fracture of which amino acids to count in the sequence
+
+    Return:
+    - dict: a dictionary with amino acids and its fracture or percentage
+    """
+
+    if show_as_percentage:
+        mult = 100
+        round_var = 2
+    else:
+        mult = 1
+        round_var = 4
+    aa_dict_count = count_aa(seq, aminoacids=aminoacids)
+    aa_dict_percent = {}
+    len_of_protein = length_of_protein(seq)
+    for aa, count in aa_dict_count.items():
+        aa_dict_percent[aa] = round(count / len_of_protein * mult, round_var)
+    return aa_dict_percent
