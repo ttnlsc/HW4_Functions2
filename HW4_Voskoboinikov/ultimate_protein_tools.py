5bbd64bd86b98d0839fbba48ac0d6abd823c8062
Aleksandr Voskoboinikov <wwoskie@gmail.com>
2023-10-01 13:56:30 +0300

    Remove unused AMINOACID_DICT const

--- HW4_Voskoboinikov/ultimate_protein_tools.py
+++ HW4_Voskoboinikov/ultimate_protein_tools.py
@@ -1,26 +1,3 @@
-AMINOACID_DICT = {
-    'A': 'Alanine', 'a': 'alanine',
-    'C': 'Cysteine', 'c': 'cysteine',
-    'D': 'Aspartic acid', 'd': 'aspartic acid',
-    'E': 'Glutamic acid', 'e': 'glutamic acid',
-    'F': 'Phenylalanine', 'f': 'Phenylalanine',
-    'G': 'Glycine', 'g': 'glycine',
-    'H': 'Histidine', 'h': 'histidine',
-    'I': 'Isoleucine', 'i': 'isoleucine',
-    'K': 'Lysine', 'k': 'lysine',
-    'L': 'Leucine', 'l': 'leucine',
-    'M': 'Methionine', 'm': 'methionine',
-    'N': 'Asparagine', 'n': 'asparagine',
-    'P': 'Proline', 'p': 'proline',
-    'Q': 'Glutamine', 'q': 'glutamine',
-    'R': 'Arginine', 'r': 'arginine',
-    'S': 'Serine', 's': 'serine',
-    'T': 'Threonine', 't': 'threonine',
-    'V': 'Valine', 'v': 'valine',
-    'W': 'Tryptophan', 'w': 'tryptophan',
-    'Y': 'Tyrosine', 'y': 'tyrosine'
-}
-
 H2O_WEIGHT: float = 18.01468
 
 AA_MASS_DICT = {
